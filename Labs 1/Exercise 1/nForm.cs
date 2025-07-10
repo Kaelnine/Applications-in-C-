a0e81820b94443eeac7cc76f1e974d437b85782f
Kaelnine <adamchuk1985@mail.ru>
2025-07-10 22:24:20 +0300

    Ex1Complete

---
+++ Labs 1/Exercise 1/nForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise_1
+{
+    public partial class nForm : Form
+    {
+        public nForm()
+        {
+            InitializeComponent();
+        }
+
+        private void nForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (nclose) return;
+            //if (checkBoxClose.Checked) return;
+            e.Cancel = true;
+            Hide();
+        }
+
+
+        public new void Close()
+        {
+            nclose = true;
+            base.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
