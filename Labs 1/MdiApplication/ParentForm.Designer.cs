f30a14e891063d21164eef6a3111e155a86f3fd8
Kaelnine <adamchuk1985@mail.ru>
2025-07-14 08:35:12 +0300

    2 exercise

---
+++ Labs 1/MdiApplication/ParentForm.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace MdiApplication
+{
+    partial class ParentForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            MdiMenu = new MenuStrip();
+            FileMenuItem = new ToolStripMenuItem();
+            NewMenuItem = new ToolStripMenuItem();
+            ExitMenuItem = new ToolStripMenuItem();
+            WindowMenuItem = new ToolStripMenuItem();
+            WindowCascadeMenuItem = new ToolStripMenuItem();
+            MdiMenu.SuspendLayout();
+            SuspendLayout();
+            // 
+            // MdiMenu
+            // 
+            MdiMenu.Items.AddRange(new ToolStripItem[] { FileMenuItem, WindowMenuItem });
+            MdiMenu.Location = new Point(0, 0);
+            MdiMenu.Name = "MdiMenu";
+            MdiMenu.Size = new Size(404, 24);
+            MdiMenu.TabIndex = 1;
+            MdiMenu.Text = "menuStrip1";
+            // 
+            // FileMenuItem
+            // 
+            FileMenuItem.DropDownItems.AddRange(new ToolStripItem[] { NewMenuItem, ExitMenuItem });
+            FileMenuItem.Name = "FileMenuItem";
+            FileMenuItem.Size = new Size(37, 20);
+            FileMenuItem.Text = "&File";
+            // 
+            // NewMenuItem
+            // 
+            NewMenuItem.Name = "NewMenuItem";
+            NewMenuItem.Size = new Size(180, 22);
+            NewMenuItem.Text = "&New";
+            // 
+            // ExitMenuItem
+            // 
+            ExitMenuItem.Name = "ExitMenuItem";
+            ExitMenuItem.Size = new Size(180, 22);
+            ExitMenuItem.Text = "&Exit";
+            ExitMenuItem.Click += ExitMenuItem_Click;
+            // 
+            // WindowMenuItem
+            // 
+            WindowMenuItem.DropDownItems.AddRange(new ToolStripItem[] { WindowCascadeMenuItem });
+            WindowMenuItem.Name = "WindowMenuItem";
+            WindowMenuItem.Size = new Size(63, 20);
+            WindowMenuItem.Text = "&Window";
+            // 
+            // WindowCascadeMenuItem
+            // 
+            WindowCascadeMenuItem.Name = "WindowCascadeMenuItem";
+            WindowCascadeMenuItem.Size = new Size(180, 22);
+            WindowCascadeMenuItem.Text = "&Cascade";
+            // 
+            // ParentForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(404, 281);
+            Controls.Add(MdiMenu);
+            IsMdiContainer = true;
+            MainMenuStrip = MdiMenu;
+            Name = "ParentForm";
+            Text = "Parent Form";
+            MdiMenu.ResumeLayout(false);
+            MdiMenu.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip MdiMenu;
+        private ToolStripMenuItem FileMenuItem;
+        private ToolStripMenuItem NewMenuItem;
+        private ToolStripMenuItem ExitMenuItem;
+        private ToolStripMenuItem WindowMenuItem;
+        private ToolStripMenuItem WindowCascadeMenuItem;
+    }
+}
---
+++ Labs 1/MdiApplication/ParentForm.cs
@@ -0,0 +1,18 @@
+namespace MdiApplication
+{
+    public partial class ParentForm : Form
+    {
+        public ParentForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        
+    }
+
+}
